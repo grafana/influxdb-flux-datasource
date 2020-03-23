fb3853b58b8b7b146057b1107e71efc23db04a13
Stephanie Closson <srclosson@gmail.com>
2020-03-23 13:51:48 -0600

    WIP: monaco editor being found, but not displayed

--- src/editor/editor_component.tsx
+++ src/editor/editor_component.tsx
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 
-import coreModule from 'grafana/app/core/core_module';
-
-import FluxMonacoEditor from './FluxMonacoEditor';
+import FluxEditorMonaco from '../libs/influx-ui';
 
 /**
  * Angular wrapper around the FLux query field
  */
-class Editor extends Component<any, any> {
+export class Editor extends Component<any, any> {
   constructor(props) {
     super(props);
     this.state = {
@@ -34,20 +32,19 @@ class Editor extends Component<any, any> {
   };
 
   render() {
-    //const { database, request } = this.props;
     const { edited, query } = this.state;
 
     return (
       <div className="gf-form-input" style={{ height: 'initial' }}>
-        <FluxMonacoEditor script={edited ? null : query} onChangeScript={this.handleChangeQuery} />
+        <FluxEditorMonaco script={edited ? null : query} onChangeScript={this.handleChangeQuery} />
       </div>
     );
   }
 }
 
-coreModule.directive('fluxEditor', [
-  'reactDirective',
-  (reactDirective: any) => {
-    return reactDirective(Editor, ['change', 'database', 'execute', 'query', 'request']);
-  },
-]);
+// coreModule.directive('fluxEditor', [
+//   'reactDirective',
+//   reactDirective => {
+//     return reactDirective(Editor, ['change', 'database', 'execute', 'query', 'request']);
+//   },
+// ]);
