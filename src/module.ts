a0085d2c7f8d14a8711c8977a7bb95e79120fd5b
Ryan McKinley <ryantxu@gmail.com>
2020-03-23 16:01:36 -0700

    with backend

--- src/module.ts
+++ src/module.ts
@@ -1,14 +1,12 @@
-import InfluxDatasource from './datasource';
-import { InfluxFluxQueryCtrl } from './query_ctrl';
-import { InfluxConfigCtrl } from './config_ctrl';
+import { DataSourcePlugin } from "@grafana/data";
+import { DataSource } from "./DataSource";
+import { QueryEditor, ConfigEditor } from "./components";
+import { InfluxQuery, InfluxOptions } from "./types";
 
-class InfluxAnnotationsQueryCtrl {
-  static templateUrl = 'partials/annotations.editor.html';
-}
-
-export {
-  InfluxDatasource as Datasource,
-  InfluxFluxQueryCtrl as QueryCtrl,
-  InfluxConfigCtrl as ConfigCtrl,
-  InfluxAnnotationsQueryCtrl as AnnotationsQueryCtrl,
-};
+export const plugin = new DataSourcePlugin<
+  DataSource,
+  InfluxQuery,
+  InfluxOptions
+>(DataSource)
+  .setConfigEditor(ConfigEditor)
+  .setQueryEditor(QueryEditor);
