82274b450211daea3409e78144212b0bd3c84961
Ryan McKinley <ryantxu@gmail.com>
2020-04-25 10:28:21 -0700

    update to 55

--- go.mod
+++ go.mod
@@ -3,9 +3,6 @@ module github.com/grafana/influx-datasource
 go 1.14
 
 require (
-	github.com/grafana/grafana-plugin-sdk-go v0.31.0
-	github.com/influxdata/influxdb-client-go v0.1.5
-	github.com/magefile/mage v1.9.0
-	github.com/patrickmn/go-cache v2.1.0+incompatible
-	github.com/prometheus/client_golang v1.3.0
+	github.com/grafana/grafana-plugin-sdk-go v0.55.0
+	github.com/influxdata/influxdb-client-go v1.1.0
 )
--- pkg/main.go
+++ pkg/main.go
@@ -1,25 +1,18 @@
 package main
 
 import (
-	"net/http"
 	"os"
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
-	"github.com/grafana/grafana-plugin-sdk-go/backend/resource/httpadapter"
+	"github.com/grafana/grafana-plugin-sdk-go/experimental"
 )
 
 func main() {
-	_ = backend.SetupPluginEnvironment("influx-datasource")
+	// Setup the plugin environment
+	backend.SetupPluginEnvironment("influx-datasource")
 
-	mux := http.NewServeMux()
-	ds := NewDataSource(mux)
-	httpResourceHandler := httpadapter.New(mux)
-
-	err := backend.Serve(backend.ServeOpts{
-		CallResourceHandler: httpResourceHandler,
-		QueryDataHandler:    ds,
-		CheckHealthHandler:  ds,
-	})
+	host := experimental.NewInstanceManager(&InfluxHost{})
+	err := host.RunGRPCServer()
 	if err != nil {
 		backend.Logger.Error(err.Error())
 		os.Exit(1)
