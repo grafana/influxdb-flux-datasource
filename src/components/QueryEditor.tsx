64253063f929379378106988a619240fbdeb4903
Ryan McKinley <ryantxu@gmail.com>
2020-05-14 12:27:10 -0700

    sample queries

--- src/components/QueryEditor.tsx
+++ src/components/QueryEditor.tsx
@@ -1,8 +1,9 @@
 import React, { PureComponent } from 'react';
-import { QueryEditorProps } from '@grafana/data';
+import { QueryEditorProps, SelectableValue } from '@grafana/data';
 import Editor from '@monaco-editor/react';
 import { DataSource } from '../DataSource';
 import { InfluxQuery, InfluxOptions } from '../types';
+import { Select, LinkButton, InlineFormLabel } from '@grafana/ui';
 // import('@influxdata/flux-lsp-browser').then(({ Server }) => {
 //   const srv = new Server(false, true);
 //   console.log('lsp server ready', srv);
@@ -10,45 +11,72 @@ import { InfluxQuery, InfluxOptions } from '../types';
 
 type Props = QueryEditorProps<DataSource, InfluxQuery, InfluxOptions>;
 
+const samples: Array<SelectableValue<string>> = [
+  { label: 'Show buckets', description: 'List the avaliable buckets (table)', value: 'buckets()' },
+  {
+    label: 'Simple  query',
+    description: 'filter by measurment and field',
+    value: `from(bucket: "db/rp")
+  |> range(start: v.timeRangeStart, stop:timeRangeStop)
+  |> filter(fn: (r) =>
+    r._measurement == "example-measurement" and
+    r._field == "example-field"
+  )`,
+  },
+];
+
 export class QueryEditor extends PureComponent<Props> {
   getEditorValue: any | undefined;
+
   onRawQueryChange = () => {
     this.props.onChange({
       ...this.props.query,
       query: this.getEditorValue(),
     });
+    this.props.onRunQuery();
   };
 
   onEditorDidMount = (getEditorValue: any) => {
     this.getEditorValue = getEditorValue;
-    //monaco.languages.register({ id: 'flux' });
-    // monaco.languages.setMonarchTokensProvider('flux', {
-    //   keywords: ['from', 'range', 'filter', 'to'],
-    //   tokenizer: {
-    //     root: [
-    //       [
-    //         /[a-z_$][\w$]*/,
-    //         {cases: {'@keywords': 'keyword', '@default': 'identifier'}},
-    //       ],
-    //     ],
-    //   },
-    // })
+  };
+
+  onSampleChange = (val: SelectableValue<string>) => {
+    this.props.onChange({
+      ...this.props.query,
+      query: val.value!,
+    });
+    this.props.onRunQuery();
   };
 
   render() {
     const { query } = this.props;
     return (
-      <>
+      <div>
         <div onBlur={this.onRawQueryChange}>
           <Editor
-            height={'300px'}
+            height={'250px'}
             language="flux"
             value={query.query}
             editorDidMount={this.onEditorDidMount}
             theme={'dark'}
           />
         </div>
-      </>
+        <div className="gf-form-inline">
+          <div className="gf-form">
+            <InlineFormLabel width={6} tooltip="This supports queries copied from chronograph">
+              Help
+            </InlineFormLabel>
+            <Select width={20} options={samples} placeholder="Sample Query" onChange={this.onSampleChange} />
+            <LinkButton
+              icon="external-link-alt"
+              variant="secondary"
+              href="https://docs.influxdata.com/flux/latest/introduction/getting-started/"
+            >
+              Flux Docs
+            </LinkButton>
+          </div>
+        </div>
+      </div>
     );
   }
 }
